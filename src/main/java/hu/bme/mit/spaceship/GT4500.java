6816c79f1743e62b3875bb45f08389f1be6b27f4
Zayyy01 <gzaymus@gmail.com>
2017-03-24 01:41:56 -0700

    another comment in the All section

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -76,7 +76,7 @@ public class GT4500 implements SpaceShip {
         }
         break;
 
-      case ALL:
+      case ALL:// ez egy másik komment
         // try to fire both of the torpedos
     		  for(int i = 1; i <= primaryTorpedoStore.getNumberOfTorpedos(); i++) {
     			  firingSuccess = primaryTorpedoStore.fire(i);
